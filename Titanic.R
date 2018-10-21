de0ad23faac9ee52e782bca120a7e83be506b613
Aymeric97 <aymeric.lechevranton@wanadoo.fr>
2018-10-21 19:01:12 +0200

    Revert "Revert "Initial commit""
    This reverts commit 4302e811f4ce5ed19696fbc300b6df92a66a1abe.

---
+++ Titanic.R
@@ -0,0 +1,12 @@
+##KaggleTitanic
+
+dtrain <- read.table(file.choose(),header = TRUE,sep=",",dec=".",quote='"',fill=TRUE)
+dtest <- read.table(file.choose(), header = TRUE, sep = ",", dec=".",quote='"')
+dgs <- read.table(file.choose(), header = TRUE, sep = ",", dec=".",quote='"')
+
+dtrain
+dtest
+dgs
+
+
+
